2a5c36a8423bf2da456f2f254f731596efb529bf
Marc <Marc@172.24.1.19>
2013-12-16 18:25:07 +0100

    IGrafica a mitges

--- src/generadordellistes/FrmGeneradorDeLlistes.java
+++ src/generadordellistes/FrmGeneradorDeLlistes.java
@@ -96,7 +96,7 @@ public class FrmGeneradorDeLlistes extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        jFileChooser1.OPEN_DIALOG;
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
